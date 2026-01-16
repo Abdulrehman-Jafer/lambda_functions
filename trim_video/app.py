6fdb1d3520f67f0af36eebb90df7b55adb686a81
Abdulrehman Jafer <abdulrehmanjaferworks01233@gmail.com>
2026-01-16 19:37:23 +0500

    feat: flask server init

---
+++ trim_video/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from trim_video import lambda_handler
+
+app = Flask(__name__)
+
+@app.route("/trim", methods=["POST"])
+def trim_video():
+    body = request.get_json(force=True)
+    return lambda_handler({"body": body},{})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
